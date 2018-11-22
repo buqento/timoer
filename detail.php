c4bfc946af3432ebf00e55fe5e381e93575dd6c2
Yusuf Yacob Ona Ola <yusuf.yacob@docotel.com>
2018-11-22 10:13:44 +0700

    new

---
+++ detail.php
@@ -0,0 +1,184 @@
+<?php
+include("fun.php");
+
+$url=$_GET['id'];
+$pecah = explode("/",$url);
+$id = $pecah[0];
+$tseo = $pecah[1];
+
+$sql = "SELECT id, judul, konten, kategori_id, deskripsi, foto_file, created_at FROM artikel WHERE id=$id";
+$result = $con->query($sql);
+
+if ($result) {
+  $row = $result->fetch_assoc();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta property="og:url" content="http://timoer.info/berita/<?= $id ?>/<?= $tseo ?>" />
+    <meta property="og:type" content="article" />
+    <meta property="og:title" content="<?= $row['judul'] ?>" />
+    <meta property="og:description" content="<?= $row['deskripsi'] ?>" />
+    <meta property="og:image" content="http://dpm-ptsp-kabmbd.com/timoer-backend/web/images/<?= $row['foto_file'] ?>" />
+    <meta property="fb:app_id" content="314574428640745" />
+    <meta name="author" content="bvqento">
+
+    <title><?= $row['judul'] ?></title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="../../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="../../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+
+    <!-- Custom styles for this template -->
+    <link href="../../css/clean-blog.min.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+  <div id="fb-root"></div>
+  <script>(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.2&appId=387708794632040&autoLogAppEvents=1';
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));</script>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand" href="../../index.php">TIMOER</a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          Menu
+          <i class="fas fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="../../index.php">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" target="_BLANK" href="https://www.facebook.com/TIMOERinfo-373759446502412/">Facebook Page</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!-- Page Header -->
+    <header class="masthead" style="background-image: url('http://dpm-ptsp-kabmbd.com/timoer-backend/web/images/<?= $row['foto_file'] ?>')">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 col-md-10 mx-auto">
+            <div class="post-heading">
+              <h1><?= $row['judul'] ?></h1>
+              <!-- <h2 class="subheading">Problems look mighty small from 150 miles up</h2> -->
+              <span class="meta">Posted by
+                <a href="#">Admin</a>
+                on </i><?= tglIndo($row["created_at"]) ?> | <?= getKategori($row["kategori_id"], $con) ?></span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+
+    <!-- Post Content -->
+    <article>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 col-md-10 mx-auto">
+
+              <?= $row["konten"] ?>
+              
+              <div class="post-preview">
+
+                  <?php
+                  $sql = "SELECT id, judul, deskripsi, foto_file FROM artikel WHERE id <> $id ORDER BY rand() DESC LIMIT 1";
+                  $result = $con->query($sql);
+                  $row = $result->fetch_assoc();
+                  ?>
+                    <?php
+                    $id = $row['id'];
+                    $t = seo_title($row['judul']).'.html';
+                    ?>
+                    <?php 
+                    echo "<a href='../../berita/$id/$t'>";
+                    ?>
+                    <h3 class="post-subtitle">
+                    <?php echo $row['judul'];?> ...
+                    </h3>
+                    <?php echo "</a>"; ?>
+
+              </div>
+
+              <div class="bottom-article">
+
+                <div class="fb-share-button" 
+                  data-href="http://timoer.info/berita/<?= $id ?>/<?= $tseo ?>" 
+                  data-layout="button_count">
+                </div>
+                
+              </div>
+
+              <div class="fb-comments" data-href="http://timoer.info/<?= $tseo ?>" data-numposts="10" data-width="650"></div>
+
+
+          </div>
+        </div>
+      </div>
+    </article>
+
+    <hr>
+
+    <!-- Footer -->
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 col-md-10 mx-auto">
+            <ul class="list-inline text-center">
+              <li class="list-inline-item">
+                <a href="#">
+                  <span class="fa-stack fa-lg">
+                    <i class="fas fa-circle fa-stack-2x"></i>
+                    <i class="fab fa-twitter fa-stack-1x fa-inverse"></i>
+                  </span>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a class="nav-link" target="_BLANK" href="https://www.facebook.com/TIMOERinfo-373759446502412/">
+                  <span class="fa-stack fa-lg">
+                    <i class="fas fa-circle fa-stack-2x"></i>
+                    <i class="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
+                  </span>
+                </a>
+              </li>
+            </ul>
+            <p class="copyright text-muted">Copyright &copy; TIMOER.info 2018</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="../../vendor/jquery/jquery.min.js"></script>
+    <script src="../../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="../../js/clean-blog.min.js"></script>
+
+  </body>
+
+</html>
---
+++ fun.php
@@ -0,0 +1,83 @@
+<?php
+
+$con = mysqli_connect("localhost","dpmptspk_timoer","manggureb3","dpmptspk_timoer");
+// $con = mysqli_connect("localhost","root","","timoer");
+if (mysqli_connect_errno()){echo "Failed to connect to MySQL: " . mysqli_connect_error();}
+
+function tglIndo($tgl){
+	$tanggal = substr($tgl,8,2);
+	$bulan = getBulan(substr($tgl,5,2));
+	$tahun = substr($tgl,0,4);
+	return $tanggal.' '.$bulan.' '.$tahun;		 
+}	
+
+function getBulan($bln){
+	switch ($bln){
+		case 1: 
+			return "Jan";
+			break;
+		case 2:
+			return "Feb";
+			break;
+		case 3:
+			return "Mar";
+			break;
+		case 4:
+			return "Apr";
+			break;
+		case 5:
+			return "Mei";
+			break;
+		case 6:
+			return "Jun";
+			break;
+		case 7:
+			return "Jul";
+			break;
+		case 8:
+			return "Agu";
+			break;
+		case 9:
+			return "Sep";
+			break;
+		case 10:
+			return "Okt";
+			break;
+		case 11:
+			return "Nov";
+			break;
+		case 12:
+			return "Des";
+			break;
+	}
+} 
+
+function getKategori($id, $con){
+	$sql = "SELECT name FROM kategori WHERE id=$id";
+	$result = $con->query($sql);
+	if ($result) {
+	  $row = $result->fetch_assoc();
+	}
+
+	return $row['name'];
+}
+
+function getJmlKategori($kategori_id, $con){
+	$sql = "SELECT COUNT(id) AS jumlah FROM artikel WHERE kategori_id=$kategori_id";
+	$result = $con->query($sql);
+	if ($result) {
+	  $row = $result->fetch_assoc();
+	}
+
+	return $row['jumlah'];
+}
+
+function seo_title($s) {
+    $c = array (' ');
+    $d = array ('-','/','\\',',','.','#',':',';','\'','"','[',']','{','}',')','(','|','`','~','!','@','%','$','^','&','*','=','?','+');
+
+    $s = str_replace($d, '', $s); // Hilangkan karakter yang telah disebutkan di array $d
+    
+    $s = strtolower(str_replace($c, '-', $s)); // Ganti spasi dengan tanda - dan ubah hurufnya menjadi kecil semua
+    return $s;
+}
