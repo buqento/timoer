5cf029a1048b88c423dfc73fbab99fd77d6096b7
Yusuf Yacob Ona Ola <yusuf.yacob@docotel.com>
2018-11-26 08:15:10 +0700

    update

--- detail.php
+++ detail.php
@@ -48,6 +48,14 @@ if ($result) {
     <!-- Custom styles for this template -->
     <link href="../../css/clean-blog.min.css" rel="stylesheet">
 
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <script>
+         (adsbygoogle = window.adsbygoogle || []).push({
+              google_ad_client: "ca-pub-1434074630735871",
+              enable_page_level_ads: true
+         });
+    </script>
+
   </head>
 
   <body>
